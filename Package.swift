9773b2af2a53134a7157a236bb93d0cc2306cba0
Todd Rylaarsdam <todd@toddr.org>
2022-12-13 23:50:56 -0800

    syntax highlighting

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.0")
+				.package(url: "https://github.com/JohnSundell/Splash", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,6 +28,6 @@ let package = Package(
             dependencies: []),
         .target(
             name: "MarkdownSwiftUI",
-            dependencies: ["RoomTime", "Yams"]),
+            dependencies: ["RoomTime", "Yams", "Splash"]),
     ]
 )
--- Sources/MarkdownSwiftUI/views/Code.swift
+++ Sources/MarkdownSwiftUI/views/Code.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Splash
 
 public struct Code: View {
 		@Environment(\.colorScheme) var colorScheme
@@ -17,43 +18,52 @@ public struct Code: View {
     }
     
     public var body: some View {
-        ZStack(alignment: .topTrailing) {
-            ZStack {
-                HStack {
-                    VStack(alignment: .trailing, spacing: 5) {
-                        ForEach(0..<element.lines.count) { i in
-                            Text("\(i + 1)")
-                                // .foregroundColor(Color.black.opacity(0.5))
-                        }
-                    }
-                    .padding([.vertical, .leading])
-                    .padding(.trailing, 7)
+			ScrollView(.horizontal) {
+				VStack(alignment: .leading, spacing: 5) {
+						ForEach(element.lines, id: \.self) { line in
+								Text(SyntaxHighlighter(format: NSAttributedString()).highlight(line))
+						}
+				}
+				.fixedSize()
+				.padding(.vertical)
+			}
+    //     ZStack(alignment: .topTrailing) {
+    //         ZStack {
+    //             HStack {
+    //                 VStack(alignment: .trailing, spacing: 5) {
+    //                     ForEach(0..<element.lines.count) { i in
+    //                         Text("\(i + 1)")
+    //                             // .foregroundColor(Color.black.opacity(0.5))
+    //                     }
+    //                 }
+    //                 .padding([.vertical, .leading])
+    //                 .padding(.trailing, 7)
                     
-                    ScrollView(.horizontal) {
-                        VStack(alignment: .leading, spacing: 5) {
-                            ForEach(element.lines, id: \.self) { line in
-                                Text(line)
-                                    // .foregroundColor(Color.black)
-                            }
-                        }
-                        .fixedSize()
-                        .padding(.vertical)
-                    }
-                }
-            }
-            .font(.custom("menlo", size: 15))
+    //                 ScrollView(.horizontal) {
+    //                     VStack(alignment: .leading, spacing: 5) {
+    //                         ForEach(element.lines, id: \.self) { line in
+    //                             Text(line)
+    //                                 // .foregroundColor(Color.black)
+    //                         }
+    //                     }
+    //                     .fixedSize()
+    //                     .padding(.vertical)
+    //                 }
+    //             }
+    //         }
+    //         .font(.custom("menlo", size: 15))
             
-            // language rect
-            if let lang = element.lang {
-                Text(lang)
-                    // .foregroundColor(Color.black.opacity(0.3))
-                    .padding(.vertical, 8)
-                    .padding(.horizontal, 12)
-            }
-        }
-        // .background(Color(red: 246/256, green: 248/256, blue: 250/256))
-				.background(colorScheme == .dark ? Color(red: 40/256, green: 40/256, blue: 40/256) : Color(red: 246/256, green: 248/256, blue: 250/256))
-        .cornerRadius(10)
-        .shadow(radius: 1)
-    }
+    //         // language rect
+    //         if let lang = element.lang {
+    //             Text(lang)
+    //                 // .foregroundColor(Color.black.opacity(0.3))
+    //                 .padding(.vertical, 8)
+    //                 .padding(.horizontal, 12)
+    //         }
+    //     }
+    //     // .background(Color(red: 246/256, green: 248/256, blue: 250/256))
+		// 		.background(colorScheme == .dark ? Color(red: 40/256, green: 40/256, blue: 40/256) : Color(red: 246/256, green: 248/256, blue: 250/256))
+    //     .cornerRadius(10)
+    //     .shadow(radius: 1)
+    // }
 }
