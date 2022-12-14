0f28db543e79d5cd66239b151827979f42d2ca93
Todd Rylaarsdam <todd@toddr.org>
2022-12-13 23:52:22 -0800

    updates

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.0")
-				.package(url: "https://github.com/JohnSundell/Splash", from: "0.1.0")
+        .package(url: "https://github.com/JohnSundell/Splash", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
