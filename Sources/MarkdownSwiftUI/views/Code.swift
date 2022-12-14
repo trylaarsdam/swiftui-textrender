09a1fdc0fa83c2b7165810f8a2717a06ff66bafa
Todd Rylaarsdam <todd@toddr.org>
2022-12-14 00:21:49 -0800

    syntax highlighting finished

--- Sources/MarkdownSwiftUI/views/Code.swift
+++ Sources/MarkdownSwiftUI/views/Code.swift
@@ -5,8 +5,31 @@
 //  Created by Talaxy on 2021/4/12.
 //
 
+import Foundation
 import SwiftUI
 import Splash
+
+struct AttributedText: UIViewRepresentable {
+    
+    let attributedString: NSAttributedString
+    
+    init(_ attributedString: NSAttributedString) {
+        self.attributedString = attributedString
+    }
+    
+    func makeUIView(context: Context) -> UILabel {
+        let label = UILabel()
+        
+        label.lineBreakMode = .byClipping
+        label.numberOfLines = 0
+
+        return label
+    }
+    
+    func updateUIView(_ uiView: UILabel, context: Context) {
+        uiView.attributedText = attributedString
+    }
+}
 
 public struct Code: View {
 		@Environment(\.colorScheme) var colorScheme
@@ -18,52 +41,32 @@ public struct Code: View {
     }
     
     public var body: some View {
-			ScrollView(.horizontal) {
-				VStack(alignment: .leading, spacing: 5) {
-						ForEach(element.lines, id: \.self) { line in
-								Text(SyntaxHighlighter(format: NSAttributedString()).highlight(line))
-						}
-				}
-				.fixedSize()
-				.padding(.vertical)
-			}
-    //     ZStack(alignment: .topTrailing) {
-    //         ZStack {
-    //             HStack {
-    //                 VStack(alignment: .trailing, spacing: 5) {
-    //                     ForEach(0..<element.lines.count) { i in
-    //                         Text("\(i + 1)")
-    //                             // .foregroundColor(Color.black.opacity(0.5))
-    //                     }
-    //                 }
-    //                 .padding([.vertical, .leading])
-    //                 .padding(.trailing, 7)
-                    
-    //                 ScrollView(.horizontal) {
-    //                     VStack(alignment: .leading, spacing: 5) {
-    //                         ForEach(element.lines, id: \.self) { line in
-    //                             Text(line)
-    //                                 // .foregroundColor(Color.black)
-    //                         }
-    //                     }
-    //                     .fixedSize()
-    //                     .padding(.vertical)
-    //                 }
-    //             }
-    //         }
-    //         .font(.custom("menlo", size: 15))
+        
+         ZStack(alignment: .topTrailing) {
+             ScrollView(.horizontal) {
+                 ScrollView(.vertical) {
+                     VStack(alignment: .leading, spacing: 5) {
+                         ForEach(element.lines, id: \.self) { line in
+                             AttributedText(SyntaxHighlighter(format: AttributedStringOutputFormat(theme: .wwdc18(withFont: Splash.Font(size: UIFont.preferredFont(forTextStyle: .body).pointSize)))).highlight(line))
+                         }
+                     }.padding()
+                 }
+             }
+             .frame(maxWidth: .infinity)
+            .padding(.vertical)
             
-    //         // language rect
-    //         if let lang = element.lang {
-    //             Text(lang)
-    //                 // .foregroundColor(Color.black.opacity(0.3))
-    //                 .padding(.vertical, 8)
-    //                 .padding(.horizontal, 12)
-    //         }
-    //     }
-    //     // .background(Color(red: 246/256, green: 248/256, blue: 250/256))
-		// 		.background(colorScheme == .dark ? Color(red: 40/256, green: 40/256, blue: 40/256) : Color(red: 246/256, green: 248/256, blue: 250/256))
-    //     .cornerRadius(10)
-    //     .shadow(radius: 1)
-    // }
+             // language rect
+             if let lang = element.lang {
+                 Text(lang)
+                     // .foregroundColor(Color.black.opacity(0.3))
+                     .padding(.vertical, 8)
+                     .padding(.horizontal, 12)
+             }
+         }
+         // .background(Color(red: 246/256, green: 248/256, blue: 250/256))
+         .frame(maxWidth: .infinity)
+         .background(colorScheme == .dark ? Color(red: 40/256, green: 40/256, blue: 40/256) : Color(red: 246/256, green: 248/256, blue: 250/256))
+         .cornerRadius(10)
+         .shadow(radius: 1)
+     }
 }
